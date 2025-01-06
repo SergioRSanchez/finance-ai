5b57d3f1bf0742d3f23d8e6ff9582cb019d3b12d
Sergio Sanchez <ssanchezfilho@gmail.com>
2025-01-06 19:44:33 -0300

    feat: ensure transaction table labels are rendered properly

--- app/transaction/_columns/index.tsx
+++ app/transaction/_columns/index.tsx
@@ -1,8 +1,36 @@
 "use client";
 
-import { Transaction } from "@prisma/client";
+import {
+  Transaction,
+  TransactionCategory,
+  TransactionPaymentMethod,
+} from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
 import TransactionTypeBadge from "../_components/type-badge";
+import { Button } from "@/app/_components/ui/button";
+import { PencilIcon, TrashIcon } from "lucide-react";
+
+export const TRANSACTION_CATEGORY_LABEL = {
+  [TransactionCategory.HOUSING]: "Moradia",
+  [TransactionCategory.EDUCATION]: "Educação",
+  [TransactionCategory.ENTERTAINMENT]: "Entretenimento",
+  [TransactionCategory.FOOD]: "Alimentação",
+  [TransactionCategory.HEALTH]: "Saúde",
+  [TransactionCategory.OTHER]: "Outros",
+  [TransactionCategory.SALARY]: "Salário",
+  [TransactionCategory.TRANSPORTATION]: "Transporte",
+  [TransactionCategory.UTILITY]: "Utilidades",
+};
+
+export const TRANSACTION_PAYMENT_METHOD_LABELS = {
+  [TransactionPaymentMethod.BANK_TRANSFER]: "Transferência",
+  [TransactionPaymentMethod.BANK_SLIP]: "Boleto",
+  [TransactionPaymentMethod.CASH]: "Dinheiro",
+  [TransactionPaymentMethod.CREDIT_CARD]: "Cartão de Crédito",
+  [TransactionPaymentMethod.DEBIT_CARD]: "Cartão de Débito",
+  [TransactionPaymentMethod.OTHER]: "Outros",
+  [TransactionPaymentMethod.PIX]: "Pix",
+};
 
 export const transactionColumns: ColumnDef<Transaction>[] = [
   {
@@ -19,21 +47,48 @@ export const transactionColumns: ColumnDef<Transaction>[] = [
   {
     accessorKey: "category",
     header: "Categoria",
+    cell: ({ row: { original: transaction } }) =>
+      TRANSACTION_CATEGORY_LABEL[transaction.category],
   },
   {
     accessorKey: "paymentMethod",
     header: "Método",
+    cell: ({ row: { original: transaction } }) =>
+      TRANSACTION_PAYMENT_METHOD_LABELS[transaction.paymentMethod],
   },
   {
     accessorKey: "date",
     header: "Data",
+    cell: ({ row: { original: transaction } }) =>
+      new Date(transaction.date).toLocaleDateString("pt-BR", {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+      }),
   },
   {
     accessorKey: "amount",
     header: "Valor",
+    cell: ({ row: { original: transaction } }) =>
+      new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      }).format(Number(transaction.amount)),
   },
   {
     accessorKey: "actions",
     header: "",
+    cell: () => {
+      return (
+        <div className="space-x-1">
+          <Button variant="ghost">
+            <PencilIcon size="icon" className="text-muted-foreground" />
+          </Button>
+          <Button variant="ghost">
+            <TrashIcon size="icon" className="text-muted-foreground" />
+          </Button>
+        </div>
+      );
+    },
   },
 ];
